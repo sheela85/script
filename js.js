9a554a21ecb3538beb65d2e1e64cb89e77e99540
sheela85 <sheela.oreops@gmail.com>
2025-02-14 17:04:50 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+
+function vfun(){
+    var uname=document.forms["myform"]["uname"].value;
+    var upswd=document.forms["myform"]["upswd"].value;
+    if(uname==null || uname==""){
+        document.getElementById("errorbox").innerHTML="enter the user name";
+        return false;
+    }
+       if(upswd==null || upswd==""){
+        document.getElementById("errorbox").innerHTML="enter the password";
+        return false;
+       }
+       if (uname !=''&& upswd !=''){
+        alert("login successfully");
+       }
+    
+}
+
+function vfun1(){
+    var uname1=document.forms["myform2"]["uname1"].value;
+    var email1=document.forms["myform2"]["email1"].value;
+    var upswd1=document.forms["myform2"]["upswd1"].value;
+    var upswd2=document.forms["myform2"]["upswd2"].value;
+
+    if(uname1==null || uname1==""){
+        document.getElementById("errorbox").innerHTML="enter the user name";
+        return false;
+    }
+    if(email1==null || email1==""){ document.getElementById("errorbox").innerHTML="enter the email1";
+        return false;
+    }
+       if(upswd1==null || upswd1==""){
+        document.getElementById("errorbox").innerHTML="enter the password";
+        return false;
+       }
+       if(upswd2==null || upswd2==""){
+        document.getElementById("errorbox").innerHTML="enter the password";
+        return false;}
+       if(upswd1!=upswd2){
+        document.getElementById("errorbox").innerHTML="password dont match";
+        return false;}
+        if(uname1 !='' && upswd1 !=''&& upswd2 !=''&& email1)
+            window.alert("Register successfull");
+       }
+
